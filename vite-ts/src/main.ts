1d2bc5d632f3de3232ef6cb6e4d70c23649c2de4
koyo code <koyo20010815@gmail.com>
2024-01-07 21:43:16 +0900

    面白い

--- vite-ts/src/main.ts
+++ vite-ts/src/main.ts
@@ -1,24 +1,83 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+let testString: string = "hello";
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+let stringOrNumber: string | number;
+stringOrNumber = 32;
+
+//array
+
+let names = ["john", "jane", "tom"];
+
+names.push("hhh");
+
+let numbers = [22, 45, 35];
+
+let stringOrNumberArray: (string | number)[] = ["hello", 43, 22];
+
+//object
+
+let user = {
+  username: "john",
+  age: 22,
+  isAdmin: false,
+};
+
+user.username = "jane";
+user.age = 29;
+user.isAdmin = true;
+
+// user.phone = "+12345677";
+let userObj: {
+  username: string;
+  age: number;
+  isAdmin: boolean;
+};
+
+userObj = {
+  username: "john",
+  age: 23,
+  isAdmin: true,
+  // phone: "ngeiru",
+};
+
+let userObj2: {
+  username: string;
+  age: number;
+  isAdmin: boolean;
+  phone?: string;
+};
+
+userObj2 = {
+  username: "hello",
+  age: 44,
+  isAdmin: true,
+  phone: "jel",
+};
+
+// functions
+
+let sayHi = () => {
+  console.log("hello");
+};
+
+let functionReturnString = (): string => {
+  console.log("hi");
+  return "h1";
+};
+
+let multiple = (num: number): number => {
+  return num * 2;
+};
+
+let sum = (num1: number, num2: number, another?: number): number => {
+  return num1 + num2;
+};
+
+sum(2, 4);
+
+let func = (user: { username: string; age: number; phone?: string }) => {
+  console.log(user.username);
+};
+
+// TYPE ALIASES
